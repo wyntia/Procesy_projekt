8ca5c2f3c40933f3b5372b6b996a4d2f4f9bfc8e
wyntia <gosia.chyrza@gmail.com>
2024-11-09 15:32:16 +0100

    initial setup

---
+++ src/main/java/pl/pollub/backend/ProcesyProjektApplication.java
@@ -0,0 +1,13 @@
+package pl.pollub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesyProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesyProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pollub/backend/ProcesyProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pollub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesyProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
